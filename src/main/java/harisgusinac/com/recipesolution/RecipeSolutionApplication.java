56ebde1b0f54e20a4e8a680eaf8ba623853e9ed8
hgusinac <hgusinac@gmail.com>
2022-02-18 08:53:41 +0100

    Initial commit

---
+++ src/main/java/harisgusinac/com/recipesolution/RecipeSolutionApplication.java
@@ -0,0 +1,13 @@
+package harisgusinac.com.recipesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/harisgusinac/com/recipesolution/RecipeSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package harisgusinac.com.recipesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
